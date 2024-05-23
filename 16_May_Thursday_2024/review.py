7fad40ce58ba300e5efeae0dd96551494fe1b3bd
Ayesha-amir <ayeshaamir0002@gmail.com>
2024-05-24 04:35:56 +0500

    24 May Friday

---
+++ 16_May_Thursday_2024/review.py
@@ -0,0 +1,58 @@
+"""
+number_1 = input("enter the first number")
+number_2 = input("enter the second number")
+
+sum = int(number_1) + int(number_2)
+
+print("the sume is: ", sum)
+"""
+
+
+"""
+for i in range (0, 10):
+  student_name = input("enter the name of student: ")
+
+print(student_name)
+
+"""
+
+print("program start")
+
+for iterator in range (0, 10):
+  print("inside loop")
+
+print("outside loop")
+
+print("program ends")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

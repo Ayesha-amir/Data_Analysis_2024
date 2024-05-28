6b412cebf950b6bc4f0e946a342c0214e8437b5c
Ayesha-amir <ayeshaamir0002@gmail.com>
2024-05-28 06:26:56 +0500

    strings

---
+++ data types official/string.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+'Hello" hilloy'
+
+"Ayesha's Bike"
+
+
+
+name = "Ayesha"
+lower_case_name = name.lower()
+
+print(lower_case_name)
+
+
+
+string = "Hello my name is Ayesha Amir, I'm a Python Developer."
+
+print(string.split(","))
+
+print("8 july 2005".split(" ")[-1])
